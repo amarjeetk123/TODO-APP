129a474e4382eecce7ef74267ea6727d9d3ebe7a
amarjeetk123 <ak123del@gmail.com>
2022-09-29 12:30:54 +0530

    adding a readme file So that anyone can get a little bit idea about my project and also doing some modification in css folder.

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-
-
-
-
 const todo_list = document.getElementsByClassName("container")[0]
 
-
 let add_btn = document.getElementsByClassName("addButton")[0]
-add_btn.addEventListener("click", () =>{
+add_btn.addEventListener("click", () => {
 
 
-    let text = document.getElementsByClassName("input")[0].value 
+    let text = document.getElementsByClassName("input")[0].value
     // console.log(text)
-    if(text == ""){
+    if (text == "") {
         alert("Please write something inside the box")
+        return;
     }
 
     const section = document.createElement("section")
     const section_input = document.createElement("input")
     section_input.classList.add("input_section")
-    section_input.setAttribute("readonly" , "readonly")
+    section_input.setAttribute("readonly", "readonly")
     section.appendChild(section_input)
     section_input.value = text
     todo_list.appendChild(section)
@@ -28,31 +24,31 @@ add_btn.addEventListener("click", () =>{
     edit_btn.innerText = "EDIT"
     const delete_btn = document.createElement("button")
     delete_btn.classList.add("delete_btn")
-    delete_btn.innerText  = "DELETE"
-   
+    delete_btn.innerText = "DELETE"
+
     section.appendChild(edit_btn)
     section.appendChild(delete_btn)
 
     // code for edit button
-    edit_btn.addEventListener("click" , ()=>{
-        if(edit_btn.innerText == "EDIT"){
+    edit_btn.addEventListener("click", () => {
+        if (edit_btn.innerText == "EDIT") {
             section_input.removeAttribute("readonly")
-        edit_btn.innerText = "SAVE"
-        section_input.focus() // it is not nessesary , this is used to blink the cursor at the end of text....
+            edit_btn.innerText = "SAVE"
+            section_input.focus() // it is not nessesary , this is used to blink the cursor at the end of text....
         }
-        else{
-            section_input.setAttribute("readonly" , "readonly")
+        else {
+            section_input.setAttribute("readonly", "readonly")
             edit_btn.innerText = "EDIT"
         }
     })
 
-// code for delete button
-delete_btn.addEventListener("click" , ()=>{
-    let user_choise = confirm("Are You Want To delete")
-    if(user_choise){
-        todo_list.removeChild(section)
-    }
-   
-})
+    // code for delete button
+    delete_btn.addEventListener("click", () => {
+        let user_choise = confirm("Are You Want To delete")
+        if (user_choise) {
+            todo_list.removeChild(section)
+        }
+
+    })
     document.getElementsByClassName("input")[0].value = ""
 })
---
+++ readme.md
@@ -0,0 +1,24 @@
+# TODO APP
+ ### I have made a Simple TODO app by the help of HTML, CSS, and JavaScript.
+
+## Technology Used:-
+  - ` HTML ` 
+  - ` CSS ` 
+  - ` JS ` 
+
+### Also The Web page has a theme Changer , So you can see the content in light mode as well as daek mode.
+
+![](https://img.shields.io/badge/Project-07-pink)
+ ![](https://img.shields.io/badge/JS-Project-yellow)
+![](https://img.shields.io/badge/HTML-5-orange)
+ ![](https://img.shields.io/badge/CSS-3-blue)
+ ![](https://img.shields.io/badge/LCO-Hitesh%20Sir-p)
+
+Live Link:- [Click here!](https://amarjeet-todo-app.netlify.app/)
+
+Picture of my project:-
+![](image.png)
+
+
+
+### Time Taken For This Project:- 2 hours.
